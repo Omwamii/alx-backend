fd03e5b5e51fede1354952cf338d187045f25570
omwami <ianalpha@protonmail.com>
2024-02-09 11:52:33 +0300

    config babel

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -13,11 +13,14 @@ class Config:
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 babel = Babel(app)
+
 
 def index():
     """ return index page """
     return render_template('1-index.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
