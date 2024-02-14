93b91e2c301c867bee3ce5a1faf0429fdd17fe1e
omwami <ianalpha@protonmail.com>
2024-02-14 16:11:02 +0300

    infer appropriate timezone

--- 0x02-i18n/7-app.py
+++ 0x02-i18n/7-app.py
@@ -71,7 +71,7 @@ def get_timezone():
 @app.route('/')
 def index():
     """ return index page """
-    return render_template('6-index.html')
+    return render_template('7-index.html')
 
 
 if __name__ == "__main__":
